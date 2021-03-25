baf875951be364d7af81a3201526e057a4664a58
TheresaPelosi <43284609+TheresaPelosi@users.noreply.github.com>
2021-03-25 16:44:35 -0400

    edit chore modal, needs chore BE

--- src/components/chore-manager/chore-display.js
+++ src/components/chore-manager/chore-display.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Button,
     Col,
     FormCheck,
@@ -6,6 +6,7 @@ import {Button,
     Row
 } from "react-bootstrap";
 import {connect} from "react-redux";
+import Chore from "./chore.js";
 
 const ChoreDisplay = ({
                         activeGroup
@@ -16,8 +17,9 @@ const ChoreDisplay = ({
 
     })
 
-    return(
+    return (
         <div>
+
             <h1>
                 {activeGroup}
             </h1>
@@ -30,87 +32,11 @@ const ChoreDisplay = ({
 
                         <br/>
 
-                        <Navbar bg="light" expand="xs">
-                            <Navbar.Text>
-                                <h3>
-                                    Chore Name
-                                </h3>
-
-                                <FormCheck style={{position: "absolute", top: "10px", right: "10px"}}/>
-
-                                Reward: Test
-
-                            </Navbar.Text>
-
-                            <Navbar.Toggle style={{position: "absolute", bottom: "10px", right: "10px"}}>View Details</Navbar.Toggle>
-
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Navbar.Text>
-                                    Due Date:
-                                    <br/>
-                                    Chore Description:
-                                    <br/>
-                                    Assignor:
-                                    <br/>
-                                    Date Added:
-                                    <br/>
-                                    Assignees:
-                                </Navbar.Text>
-
-                                <br/>
-
-                                <div style={{paddingBottom: "50px"}}>
-                                    <Button style={{marginRight: "15px"}}>
-                                        Edit Chore
-                                    </Button>
-                                    <Button variant="danger">
-                                        Delete Chore
-                                    </Button>
-                                </div>
-                            </Navbar.Collapse>
-                        </Navbar>
+                        <Chore/>
 
                         <br/>
 
-                        <Navbar bg="light" expand="xs">
-                            <Navbar.Text>
-                                <h3>
-                                    Chore Name
-                                </h3>
-
-                                <FormCheck style={{position: "absolute", top: "10px", right: "10px"}}/>
-
-                                Reward: Test
-
-                            </Navbar.Text>
-
-                            <Navbar.Toggle style={{position: "absolute", bottom: "10px", right: "10px"}}>View Details</Navbar.Toggle>
-
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Navbar.Text>
-                                    Due Date:
-                                    <br/>
-                                    Chore Description:
-                                    <br/>
-                                    Assignor:
-                                    <br/>
-                                    Date Added:
-                                    <br/>
-                                    Assignees:
-                                </Navbar.Text>
-
-                                <br/>
-
-                                <div style={{paddingBottom: "50px"}}>
-                                    <Button style={{marginRight: "15px"}}>
-                                        Edit Chore
-                                    </Button>
-                                    <Button variant="danger">
-                                        Delete Chore
-                                    </Button>
-                                </div>
-                            </Navbar.Collapse>
-                        </Navbar>
+                        <Chore/>
 
                         <br/>
 
@@ -134,87 +60,11 @@ const ChoreDisplay = ({
 
                         <br/>
 
-                        <Navbar bg="light" expand="xs">
-                            <Navbar.Text>
-                                <h3>
-                                    Chore Name
-                                </h3>
-
-                                <FormCheck style={{position: "absolute", top: "10px", right: "10px"}}/>
-
-                                Reward: Test
-
-                            </Navbar.Text>
-
-                            <Navbar.Toggle style={{position: "absolute", bottom: "10px", right: "10px"}}>View Details</Navbar.Toggle>
-
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Navbar.Text>
-                                    Due Date:
-                                    <br/>
-                                    Chore Description:
-                                    <br/>
-                                    Assignor:
-                                    <br/>
-                                    Date Added:
-                                    <br/>
-                                    Assignees:
-                                </Navbar.Text>
-
-                                <br/>
-
-                                <div style={{paddingBottom: "50px"}}>
-                                    <Button style={{marginRight: "15px"}}>
-                                        Edit Chore
-                                    </Button>
-                                    <Button variant="danger">
-                                        Delete Chore
-                                    </Button>
-                                </div>
-                            </Navbar.Collapse>
-                        </Navbar>
+                        <Chore/>
 
                         <br/>
 
-                        <Navbar bg="light" expand="xs">
-                            <Navbar.Text>
-                                <h3>
-                                    Chore Name
-                                </h3>
-
-                                <FormCheck style={{position: "absolute", top: "10px", right: "10px"}}/>
-
-                                Reward: Test
-
-                            </Navbar.Text>
-
-                            <Navbar.Toggle style={{position: "absolute", bottom: "10px", right: "10px"}}>View Details</Navbar.Toggle>
-
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Navbar.Text>
-                                    Due Date:
-                                    <br/>
-                                    Chore Description:
-                                    <br/>
-                                    Assignor:
-                                    <br/>
-                                    Date Added:
-                                    <br/>
-                                    Assignees:
-                                </Navbar.Text>
-
-                                <br/>
-
-                                <div style={{paddingBottom: "50px"}}>
-                                    <Button style={{marginRight: "15px"}}>
-                                        Edit Chore
-                                    </Button>
-                                    <Button variant="danger">
-                                        Delete Chore
-                                    </Button>
-                                </div>
-                            </Navbar.Collapse>
-                        </Navbar>
+                        <Chore/>
 
                     </Col>
                 </Row>

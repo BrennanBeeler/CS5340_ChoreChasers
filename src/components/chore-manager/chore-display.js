bb87c6b0d529ab82f1c1eefb496cd3ba0dd2dae9
[Brennan Beeler] <[beeler.b@husky.neu.edu]>
2021-03-28 22:00:39 -0400

    Made chore display filter based on date and created different area for overdue. Doesn't sort beyond overdue or not overdue.

--- src/components/chore-manager/chore-display.js
+++ src/components/chore-manager/chore-display.js
@@ -5,11 +5,43 @@ import "./chore-display.css"
 
 
 const ChoreDisplay = ({chores, deleteChore, updateProgress}) => {
+
+    const findOverdue = () => {
+        return chores.filter(chore => (chore.dueDate !== null && chore.dueDate < new Date()))
+            .sort((a, b) => {
+                if (a.dueDate < b.dueDate) {
+                    return -1
+                } else {
+                    return 1
+                }
+            });
+    }
+
     return(
         <div>
             <div className="border-top border-dark hero">
                 <Row>
                     <Col xs={6} style={{paddingLeft: "30px", paddingRight : "30px"}}>
+                        {/*TODO: break up chores by date*/}
+
+                        {
+                            findOverdue().length !== 0 &&
+                            <>
+                                <h3>
+                                    Overdue
+                                </h3>
+                                <br/>
+                                {
+                                    findOverdue().map(chore =>
+                                        <div key={chore.id}>
+                                            <ChoreCard chore={chore} deleteChore={deleteChore}
+                                                       updateProgress={updateProgress}/>
+                                                <br/>
+                                        </div>)
+                                }
+                            </>
+                        }
+
                         <h3>
                             Due Today
                         </h3>
@@ -17,7 +49,14 @@ const ChoreDisplay = ({chores, deleteChore, updateProgress}) => {
                         <br/>
 
                         {
-                            chores.map(chore => {
+                            chores.filter(chore => chore.dueDate !== null && chore.dueDate > new Date()).sort((a, b) => {
+                                if(a.dueDate < b.dueDate) {
+                                    return -1
+                                }
+                                else {
+                                    return 1
+                                }
+                            }).map(chore => {
                                     return (
                                         <div key={chore.id}>
                                             <ChoreCard chore={chore} deleteChore={deleteChore}
@@ -37,7 +76,7 @@ const ChoreDisplay = ({chores, deleteChore, updateProgress}) => {
                         <br/>
 
                         {
-                            chores.map(chore =>
+                            chores.filter(chore => chore.dueDate === null).map(chore =>
                                 <div key={chore.id}>
                                     <ChoreCard chore={chore} deleteChore={deleteChore}
                                                updateProgress={updateProgress}/>

f8162254424fcc3b9728c6194d49420de73b924b
Madhuri Palanivelu <palsmadhu18@gmail.com>
2021-03-30 03:48:31 -0700

    made changes to chore-manager and started chore-display css

--- src/components/chore-manager/chore-display.js
+++ src/components/chore-manager/chore-display.js
@@ -1,3 +1,111 @@
+// import React from "react";
+// import {Col, Row} from "react-bootstrap";
+// import ChoreCard from "./chore-card";
+// import "./chore-display.css"
+// import applicationActions from "../../actions/actions";
+// import {connect} from "react-redux";
+//
+//
+// const ChoreDisplay = ({chores, background, deleteChore, updateProgress}) => {
+//     return(
+//         <div>
+//             <div className="border-top border-dark hero" style={{backgroundImage: "url("+background+")"}}>
+//                 <Row>
+//                     <Col xs={6} style={{paddingLeft: "30px", paddingRight : "30px"}}>
+//                         {/*TODO: break up chores by date*/}
+//
+//                         {
+//                             console.log(chores)
+//                         }
+//
+//
+//                         <>
+//                             {/*TODO: decide if we want to have overdue disappear*/}
+//                             <h3>
+//                                 Overdue
+//                             </h3>
+//                             <br/>
+//                             {
+//                                 chores.filter(chore => (chore.dueDate !== null && chore.dueDate < new Date().toDateString()))
+//                                     .sort((a, b) => {
+//                                         if (a.dueDate < b.dueDate) {
+//                                             return -1
+//                                         } else {
+//                                             return 1
+//                                         }
+//                                     }).map(chore => {
+//
+//                                         return( <div key={chore.id}>
+//                                             <ChoreCard chore={chore} deleteChore={deleteChore}
+//                                                        updateProgress={updateProgress}/>
+//                                             <br/>
+//                                         </div>)
+//                                     }
+//                                    )
+//                             }
+//                         </>
+//
+//
+//                         <h3>
+//                             Due Today
+//                         </h3>
+//
+//                         <br/>
+//
+//                         {
+//                             chores.filter(chore => chore.dueDate !== null && chore.dueDate > new Date().toDateString())
+//                                 .sort((a, b) => {
+//                                     if(a.dueDate < b.dueDate) {
+//                                         return -1
+//                                     }
+//                                     else {
+//                                         return 1
+//                                     }
+//                                 }).map(chore => {
+//                                     return (
+//                                         <div key={chore.id}>
+//                                             <ChoreCard chore={chore} deleteChore={deleteChore}
+//                                                        updateProgress={updateProgress}/>
+//                                             <br/>
+//                                         </div>)
+//                                 })
+//                         }
+//                     </Col>
+//
+//                     <Col xs={6} style={{paddingRight: "30px", paddingLeft: "30px"}}>
+//                         <h3>
+//                             Undated Chores
+//                         </h3>
+//
+//                         <br/>
+//
+//                         {
+//                             chores.filter(chore => chore.dueDate === null).map(chore =>
+//                                 <div key={chore.id}>
+//                                     <ChoreCard chore={chore} deleteChore={deleteChore}
+//                                                updateProgress={updateProgress}/>
+//                                     <br/>
+//                                 </div>
+//                             )
+//                         }
+//                     </Col>
+//                 </Row>
+//             </div>
+//         </div>
+//     )
+// }
+//
+// const stpm = (state, ownProps) => ({
+//     background: state.profile.background,
+//     props: ownProps
+// })
+//
+// const dtpm = (dispatch) => ({
+//     setBackground : (url) => applicationActions.setBackground(dispatch, url)
+// })
+//
+// export default connect(stpm, dtpm)(ChoreDisplay);
+
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 import ChoreCard from "./chore-card";
@@ -35,13 +143,13 @@ const ChoreDisplay = ({chores, background, deleteChore, updateProgress}) => {
                                         }
                                     }).map(chore => {
 
-                                        return( <div key={chore.id}>
-                                            <ChoreCard chore={chore} deleteChore={deleteChore}
-                                                       updateProgress={updateProgress}/>
-                                            <br/>
-                                        </div>)
-                                    }
-                                   )
+                                               return( <div key={chore.id}>
+                                                   <ChoreCard chore={chore} deleteChore={deleteChore}
+                                                              updateProgress={updateProgress}/>
+                                                   <br/>
+                                               </div>)
+                                           }
+                                )
                             }
                         </>
 
@@ -62,13 +170,13 @@ const ChoreDisplay = ({chores, background, deleteChore, updateProgress}) => {
                                         return 1
                                     }
                                 }).map(chore => {
-                                    return (
-                                        <div key={chore.id}>
-                                            <ChoreCard chore={chore} deleteChore={deleteChore}
-                                                       updateProgress={updateProgress}/>
-                                            <br/>
-                                        </div>)
-                                })
+                                return (
+                                    <div key={chore.id}>
+                                        <ChoreCard chore={chore} deleteChore={deleteChore}
+                                                   updateProgress={updateProgress}/>
+                                        <br/>
+                                    </div>)
+                            })
                         }
                     </Col>
 
@@ -81,11 +189,11 @@ const ChoreDisplay = ({chores, background, deleteChore, updateProgress}) => {
 
                         {
                             chores.filter(chore => chore.dueDate === null).map(chore =>
-                                <div key={chore.id}>
-                                    <ChoreCard chore={chore} deleteChore={deleteChore}
-                                               updateProgress={updateProgress}/>
-                                    <br/>
-                                </div>
+                                                                                   <div key={chore.id}>
+                                                                                       <ChoreCard chore={chore} deleteChore={deleteChore}
+                                                                                                  updateProgress={updateProgress}/>
+                                                                                       <br/>
+                                                                                   </div>
                             )
                         }
                     </Col>
@@ -105,3 +213,4 @@ const dtpm = (dispatch) => ({
 })
 
 export default connect(stpm, dtpm)(ChoreDisplay);
+
--- src/components/chore-manager/chore-manager.js
+++ src/components/chore-manager/chore-manager.js
@@ -1,3 +1,119 @@
+// import React, {useState} from "react";
+// import PersonalChores from "./personal-chores";
+// import applicationActions from "../../actions/actions";
+// import {connect} from "react-redux";
+// import {Link} from "react-router-dom";
+// import GroupChores from "./group-chores";
+// import CreateGroupModal from "../create-group/create-group-modal";
+// import PendingGroup from "./pending-group";
+//
+// const ChoreManager = ({
+//                         profile,
+//                         points,
+//                         setActiveGroup,
+//                         activeGroupId = "Personal Chores",
+//                         groups,
+//                       }) => {
+//
+//     const [showCreateGroupModal, setCreateGroupModal] = useState(false);
+//     let level = 1;
+//     let maxPoints = 10;
+//     const updateLevel = () => {
+//       if (points < 10) {
+//         level = 1;
+//         maxPoints = 10;
+//       } else if (points < 100) {
+//         level = 2;
+//         maxPoints = 100;
+//       } else if (points < 1000000) {
+//         level = 3;
+//         maxPoints = 1000000;
+//       }
+//     };
+//     // TODO: It doesn't affect performance but this is pretty bad practice, change later
+//     updateLevel();
+//
+//     return (
+//         <div className="hci-full-height row">
+//             {/*TODO: figure out how to keep separator between groups and chores moving*/}
+//             <div className="col-3 border-right hci-full-height">
+//                 <div className="row-12 border-bottom border-dark pb-2">
+//                     Level {level}
+//                     <br/>
+//                     <Link to="/profile" className="btn fa fa-user-circle fa-2x"/>
+//                       {points}/{maxPoints} points
+//                     <br/>
+//
+//                     <Link to="/profile">
+//                         View {profile}'s Account
+//                     </Link>
+//                 </div>
+//
+//                 <button className="btn btn-info btn-block mt-4" onClick={() => setCreateGroupModal(true)}>
+//                     Create Group
+//                     <i className="fa fa-plus" style={{paddingLeft: "10px"}}/>
+//                 </button>
+//
+//                 <CreateGroupModal key={new Date().getTime()} show={showCreateGroupModal}
+//                                   onHide={()=> setCreateGroupModal(false)}
+//                                   profile={profile}/>
+//
+//                 <br/>
+//
+//                 <ul className="nav flex-column nav-pills mt-4" role="navigation">
+//
+//                     <li className={`nav-link nav-item mb-4 border border-dark ${activeGroupId === "Personal Chores" ? 'active':''}`}
+//                         onClick={() => setActiveGroup("Personal Chores")}>
+//                         {activeGroupId === "Personal Chores" ? "Personal Chores" : "View Personal Chores"}
+//                     </li>
+//
+//                     {
+//                         groups.map(group =>
+//                             <li className={`nav-link nav-item mb-4 border border-dark ${activeGroupId === group.id ? 'active':''}`}
+//                                 key={group.id}
+//                                 onClick={() => setActiveGroup(group.id)}>
+//                                 {activeGroupId === group.id ? group.name : "View " + group.name}
+//                             </li>
+//                         )
+//                     }
+//                 </ul>
+//             </div>
+//
+//             <div className="col-9" style={{paddingLeft: 0, paddingRight: 0}}>
+//                 {
+//                     activeGroupId === "Personal Chores" &&
+//                     <PersonalChores/>
+//
+//                 }
+//
+//                 {
+//                     activeGroupId !== "Personal Chores" &&
+//                     <>
+//                         <GroupChores/>
+//                     </>
+//                 }
+//
+//             </div>
+//         </div>
+//     )
+// }
+//
+// const stpm = (state) => ({
+//     profile: state.profile.username,
+//     points: state.profile.points,
+//     activeGroupId: state.activeGroupId,
+//     groups : state.groups,
+// })
+//
+// const dtpm = (dispatch) => ({
+//     signUp : (email, username, password) => applicationActions.signUp(dispatch, email, username, password),
+//     logIn : (email, password) => applicationActions.logIn(dispatch, email, password),
+//     setActiveGroup : (activeGroupId) => applicationActions.setActiveGroup(dispatch, activeGroupId)
+// })
+//
+// export default connect(stpm, dtpm)(ChoreManager);
+//
+
 import React, {useState} from "react";
 import PersonalChores from "./personal-chores";
 import applicationActions from "../../actions/actions";
@@ -6,50 +122,55 @@ import {Link} from "react-router-dom";
 import GroupChores from "./group-chores";
 import CreateGroupModal from "../create-group/create-group-modal";
 import PendingGroup from "./pending-group";
+import "./chore-manager.css";
 
 const ChoreManager = ({
-                        profile,
-                        points,
-                        setActiveGroup,
-                        activeGroupId = "Personal Chores",
-                        groups,
+                          profile,
+                          points,
+                          setActiveGroup,
+                          activeGroupId = "Personal Chores",
+                          groups,
                       }) => {
 
     const [showCreateGroupModal, setCreateGroupModal] = useState(false);
     let level = 1;
     let maxPoints = 10;
     const updateLevel = () => {
-      if (points < 10) {
-        level = 1;
-        maxPoints = 10;
-      } else if (points < 100) {
-        level = 2;
-        maxPoints = 100;
-      } else if (points < 1000000) {
-        level = 3;
-        maxPoints = 1000000;
-      }
+        if (points < 10) {
+            level = 1;
+            maxPoints = 10;
+        } else if (points < 100) {
+            level = 2;
+            maxPoints = 100;
+        } else if (points < 1000000) {
+            level = 3;
+            maxPoints = 1000000;
+        }
     };
     // TODO: It doesn't affect performance but this is pretty bad practice, change later
     updateLevel();
 
     return (
-        <div className="hci-full-height row">
+        <div className="hci-full-height row ">
             {/*TODO: figure out how to keep separator between groups and chores moving*/}
-            <div className="col-3 border-right hci-full-height">
-                <div className="row-12 border-bottom border-dark pb-2">
+            {/*change col-2 to col-3 below, if you want to revert back to original width of screen*/}
+            <div className="col-2 border-right hci-full-height">
+                <div className="row-12 border-bottom border-dark pb-4">
+                    <div class="level-header">
                     Level {level}
-                    <br/>
-                    <Link to="/profile" className="btn fa fa-user-circle fa-2x"/>
-                      {points}/{maxPoints} points
-                    <br/>
-
+                    </div>
+                    <div class="profile-points">
+                    <Link to="/profile" className="btn fa fa-user-circle fa-3x"/>
+                    {points}/{maxPoints} points
+                    </div>
+                    <div class="profile-link">
                     <Link to="/profile">
                         View {profile}'s Account
                     </Link>
+                    </div>
                 </div>
 
-                <button className="btn btn-info btn-block mt-4" onClick={() => setCreateGroupModal(true)}>
+                <button className="btn btn-info btn-block mt-4 pt-3 pb-3" onClick={() => setCreateGroupModal(true)}>
                     Create Group
                     <i className="fa fa-plus" style={{paddingLeft: "10px"}}/>
                 </button>
@@ -62,18 +183,22 @@ const ChoreManager = ({
 
                 <ul className="nav flex-column nav-pills mt-4" role="navigation">
 
-                    <li className={`nav-link nav-item mb-4 border border-dark ${activeGroupId === "Personal Chores" ? 'active':''}`}
+                    {/*<li className={`nav-link nav-item mb-4 border border-dark ${activeGroupId === "Personal Chores" ? 'active':''}`}*/}
+                    {/*    onClick={() => setActiveGroup("Personal Chores")}>*/}
+                    {/*    {activeGroupId === "Personal Chores" ? "Personal Chores" : "View Personal Chores"}*/}
+                    {/*</li>*/}
+                    <li className={` nav-link nav-item mb-4 pt-4 pb-4 border border-dark group-button ${activeGroupId === "Personal Chores" ? 'active':''}`}
                         onClick={() => setActiveGroup("Personal Chores")}>
                         {activeGroupId === "Personal Chores" ? "Personal Chores" : "View Personal Chores"}
                     </li>
 
                     {
                         groups.map(group =>
-                            <li className={`nav-link nav-item mb-4 border border-dark ${activeGroupId === group.id ? 'active':''}`}
-                                key={group.id}
-                                onClick={() => setActiveGroup(group.id)}>
-                                {activeGroupId === group.id ? group.name : "View " + group.name}
-                            </li>
+                                       <li className={`nav-link nav-item mb-4 pt-4 pb-4 border border-dark group-button ${activeGroupId === group.id ? 'active':''}`}
+                                           key={group.id}
+                                           onClick={() => setActiveGroup(group.id)}>
+                                           {activeGroupId === group.id ? group.name : "View " + group.name}
+                                       </li>
                         )
                     }
                 </ul>

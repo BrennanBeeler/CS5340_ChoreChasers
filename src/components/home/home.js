0f164aa30173fdb7261eef0f1c12de1c005d88f4
Madhuri Palanivelu <palsmadhu18@gmail.com>
2021-04-13 16:55:07 -0700

    added blank slideshow to home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel'
 import './home.css';
 
 const Home = () => {
@@ -46,7 +47,50 @@ const Home = () => {
                 {/*<img src="https://via.placeholder.com/500" alt="Home page" width="500" height="400" className="mx-auto"/>*/}
                 {/*<img src="/public/hangingclothes2.jpg" alt="Home page" width="4608" height="2088" className="mx-auto"/>*/}
                 <div className="image-placeholder mx-auto">
-                    <h4>TBD (Slideshow)</h4>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                // src="holder.js/800x400?text=First slide&bg=373940"
+                                alt="First slide"
+                                width="500"
+                                height="400"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                // src="holder.js/800x400?text=Second slide&bg=282c34"
+                                alt="Second slide"
+                                width="500"
+                                height="400"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                // src="holder.js/800x400?text=Third slide&bg=20232a"
+                                alt="Third slide"
+                                width="500"
+                                height="400"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                    {/*<h4>TBD (Slideshow)</h4>*/}
                 </div>
 
 
